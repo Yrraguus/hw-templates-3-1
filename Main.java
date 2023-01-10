dc7bfe3304ea08902efbe4148aae4c634ccf2178
Igor Yelshin <garry.by.ru@bk.ru>
2023-01-10 18:02:45 +0300

    added files

---
+++ Main.java
@@ -0,0 +1,13 @@
+// https://github.com/netology-code/jd-homeworks/blob/master/behave/task1/README.md
+
+public class Main {
+    public static void main(String[] args) {
+        for (int r : new Randoms(90, 100)) {
+            System.out.println("Случайное число: " + r);
+            if (r == 100) {
+                System.out.println("Выпало число 100, давайте на этом закончим");
+                break;
+            }
+        }
+    }
+}
